428654fdd44d5a1e5645d9500125c47a20e365ec
ShadowFox <38166696+ShadowFFox@users.noreply.github.com>
2022-07-05 10:58:09 -0300

    test

--- Proxy.sh
+++ Proxy.sh
@@ -20,19 +20,12 @@ echo "Fazer a instalação limpa do docker, S ou N"
 read uninstallDocker
 if [[ $uninstallDocker = "s" || $uninstallDocker = "S" ]] 
 then 
-    echo "Verificando e removendo o Docker Engine instalado\n"
-    yum remove -y docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-engine
+    echo "Verificando e removendo o Docker Engine instalado"
+    yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
 
 elif [[ $uninstallDocker = "n" || $uninstallDocker = "N" ]]
 then 
-    echo "Mantendo a instalação do Docker Engine instalado\n"
+    echo "Mantendo a instalação do Docker Engine instalado"
 else
     clear
     echo "Opção invalida, inicie novamente o script!"
@@ -47,12 +40,13 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 ## Preparação do Zabbix Proxy
+clear
 
 # Variaveis iniciais
 zabbixServer="proxy.tecnocomp.com.br"           # IP ou DNS do Zabbix Server
 versionTag="6.0-centos-latest"                  # Define a variável de versão utilizada (Padrão: 6.0 CentOS Latest)
 
-echo "Zabbix Server: $zabbixServer \n"
+echo "Zabbix Server: $zabbixServer"
 echo "Digite o Proxy Name da maquina: "
 read proxyName
 
@@ -60,16 +54,7 @@ read proxyName
 mkdir /etc/zabbix/
 
 # Instalação do Zabbix Agent2 (Opcional, o Proxy pode monitorar suas funções básicas)
-docker run --name ZabbixAgent2 \                # Nome do Container
-           --privileged \                       # Garantia de privilegios na maquina
-           -e ZBX_HOSTNAME="$proxyName" \       # Nome do Proxy (Definido préviamente no Front End do Zabbix)
-           -e ZBX_SERVER_HOST="localhost" \     # Endereço do Zabbix Server ou Proxy (Padrão: localhost)
-           -d zabbix/zabbix-agent2:$versionTag  # Imagem utilizada (Padrão: 6.0 latest centos)
+docker run --name ZabbixAgent2 --privileged -e ZBX_HOSTNAME="$proxyName" -e ZBX_SERVER_HOST="localhost" -d zabbix/zabbix-agent2:$versionTag
 
-# Instalação do Zabbix Agent2 (Opcional, o Proxy pode monitorar suas funções básicas)
-docker run --name ZabbixProxy \                 # Nome do Container
-           --privileged \                       # Garantia de privilegios na maquina
-           -e ZBX_HOSTNAME="$proxyName" \       # Nome do Proxy (Definido préviamente no Front End do Zabbix)
-           -e ZBX_SERVER_HOST="$zabbixServer" \ # Endereço do Zabbix Server ou Proxy (Padrão: localhost)
-           -d zabbix/zabbix-agent2:$versionTag  # Imagem utilizada (Padrão: 6.0 latest centos)
-
+# Instalação do Zabbix Proxy
+docker run --name ZabbixProxy --privileged -e ZBX_HOSTNAME="$proxyName" -e ZBX_SERVER_HOST="$zabbixServer" -d zabbix/zabbix-proxy-sqlite3:$versionTag
